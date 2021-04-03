5fd61544cb23c0a2fd7c2c16bfade021c1470cfb
lion009-cpu <79120389+lion009-cpu@users.noreply.github.com>
2021-04-03 13:32:41 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header classNasme="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          suddenly saved?.
         </p>
         <a
           className="App-link"
